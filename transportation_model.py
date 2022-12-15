9571846ef78c04eff0dd509119f11bd6c301c516
Moussa-Alit <moussaalit58@gmail.com>
2022-12-15 18:36:04 -0500

    Improving every func and fixing all bugs that prevent terminating work (except one)

--- transportation_model.py
+++ transportation_model.py
@@ -8,12 +8,12 @@ def get_key(val, the_dict):
 class TransportationModel():
     sources_supps = {'S1':20, 'S2':25, 'S3':30} #fill and balance method will fill this
     dests_demands = {'D1':33, 'D2':22, 'D3':11, 'D4':9} #fill and balance method will fill this
-    basic_cells = {'X11':20, 'X21':13, 'X22':12, 'X32':10, 'X33':11, 'X34':9} #non eliminated cells #we need a func that to check if we need a 0 value cell to be not eliminated
+    basic_cells = {}#{'X11':20, 'X21':13, 'X22':12, 'X32':10, 'X33':11, 'X34':9} #non eliminated cells #we need a func that to check if we need a 0 value cell to be not eliminated
     basic_cells_costs = {}
-    costs = {'X11':4, 'X12':6, 'X13':8, 'X14':20, 'X21':10, 'X22':11, 'X23':7, 'X24':7, 'X31':3, 'X32':4, 'X33':15, 'X34':1} #Will fill into this dict all the costsfor all cells
-    Us = {'U1':0, 'U2':6, 'U3':-1} #fill Us values
-    Vs = {'V1':4, 'V2':5, 'V3':16, 'V4':2} #fill Vs values
-    reduced_costs = {'X12':-1, 'X13':8, 'X14':-18, 'X23':15, 'X24':1, 'X31':0}
+    costs = {'X11':4, 'X12':6, 'X13':8, 'X14':20, 'X21':10, 'X22':11, 'X23':7, 'X24':7, 'X31':3, 'X32':4, 'X33':13, 'X34':1} #Will fill into this dict all the costsfor all cells
+    Us = {}#{'U1':0, 'U2':6, 'U3':-1} #fill Us values
+    Vs = {}#{'V1':4, 'V2':5, 'V3':14, 'V4':2} #fill Vs values
+    reduced_costs = {}#{'X12':-1, 'X13':8, 'X14':-18, 'X23':15, 'X24':1, 'X31':0}
     teta_loop_basic_cells = {} #should be global 7atta ma tsaffir bl recrusion
     c = 1
     c1 = 0
@@ -87,26 +87,16 @@ class TransportationModel():
         #what I did , I substracted indexes from the last temper dict item wich is the previous basic cell and the act cell, to be a horizantal pass or vertical pass a substraction should be 0
         #so if both are not 0 => diagonal pass; => return one source backward at the same dest and add a basic_cell 
 
-    def fill_basic_cells_costs(self, costPerunitPerunit=False): #not needed
-        for cell in self.basic_cells:
-            if costPerunitPerunit:
-                costPerunitPerunit = int(input("Enter cost per unit per unit: "))
-                unitamount = int(input("Enter unit amount: "))
-                costPerunit = costPerunitPerunit * unitamount
-            else:
-                costPerunit = int(input("Enter cost per unit: "))
-            self.basic_cells_costs[cell] = costPerunit
-
     def fill_costs(self, costPerunitPerunit=False): #costPerunitPerunit is True or False
         for s_i in range(len(self.sources_supps)): #Using range is, better for indexing
            for d_i in range(len(self.dests_demands)):
                 cell = f"X{s_i+1}{d_i+1}"
                 if costPerunitPerunit:
-                    costPerunitPerunit = int(input(f"Enter cost per unit per unit for {cell}: "))
-                    unitamount = int(input(f"Enter unit amount {cell}: "))
+                    costPerunitPerunit = float(input(f"Enter cost per unit per unit for {cell}: "))
+                    unitamount = float(input(f"Enter unit amount {cell}: "))
                     costPerunit = costPerunitPerunit * unitamount
                 else:
-                    costPerunit = int(input(f"Enter cost per unit {cell}: "))    
+                    costPerunit = float(input(f"Enter cost per unit {cell}: "))    
                 self.costs[cell] = costPerunit
         print(self.costs)
         return self.costs
@@ -130,58 +120,134 @@ class TransportationModel():
         #if all reduced costs <= 0    => optimum trasportation cost
         #loop over costs, check if eliminated cell then calculate
         for cost in self.costs: #cost is a cell index : Xij, So cost[1] => i, cost[2] => j
-            print(cost)
             if cost in self.basic_cells:
                 continue #we need only eliminated cells
             else:
                 self.reduced_costs[cost] = self.Us[f"U{cost[1]}"] + self.Vs[f"V{cost[2]}"] - self.costs[cost] #hayk bn7otton bdict la7al
-        print(self.reduced_costs)
+        print("Reduced costs: ", self.reduced_costs)
         return self.reduced_costs
 
-    def check_optimality(self):
-        if all(self.reduced_costs) <= 0:
-            return True
-        else:
-            return False
+    def optimized_cost(self):
+        optimized_cost = 0
+        for cost in self.costs: #cost is a cell
+            if cost in self.basic_cells:
+                optimized_cost += self.basic_cells[cost] * self.costs[cost]
+            else:
+                continue
+        return optimized_cost
 
-    def nearest_in_row(self, cell_to_workwith):
+    def check_optimality(self):
+        #if all(self.reduced_costs.values()) <= 0: #Surprized all() cant tets < 0 values!!!!! 
+        for reduced_cost_v in self.reduced_costs.values():
+            if reduced_cost_v > 0:
+                return False
+            else:
+                continue
+        optimized_cost = self.optimized_cost()
+        print(optimized_cost)
+        return True
+
+    def nearest_in_row(self, working_with_cell):
         distances = {}
         for cell in self.basic_cells:
-            if cell != cell_to_workwith and cell[1] == cell_to_workwith[1]: 
-                distance = int(cell[2]) - int(cell_to_workwith[2]) #distance is how many dest cell is away from entering cell
+            if cell != working_with_cell and cell[1] == working_with_cell[1]: 
+                distance = int(working_with_cell[2]) - int(cell[2])#distance is how many dest cell is away from entering cell
                 print("Distance: ", distance)
                 if distance < 0:
                     distance = -distance #we need absolute value of distance
+                distances[cell] = distance
+        if not distances:
+            #self.teta_loop_basic_cells.pop(working_with_cell)
+            #aslan ba3d ma fwwatta
+            nearest_cell_in_col = self.nearest_in_col_if_no_nearest_in_row(working_with_cell)
+            self.teta_loop_basic_cells[nearest_cell_in_col] = self.basic_cells[nearest_cell_in_col]
+            return self.nearest_in_row(nearest_cell_in_col)
+        nearest_cell = get_key(min(distances.values()), distances)
+        #print("Nearest cell in row:", nearest_cell)
+        #print("Nearest cell value: ", self.basic_cells[nearest_cell])
+        print("Nearest cell returned by nearest_in_row: ", nearest_cell)
+        return nearest_cell
+
+    def nearest_in_col_if_no_nearest_in_row(self, working_with_cell):
+        distances = {}
+        for cell in self.basic_cells:
+            if cell != working_with_cell and cell[2] == working_with_cell[2]: 
+                distance = int(working_with_cell[1]) - int(cell[1]) #distance is how many dest cell is away from entering cell
+                print("Distance: ", distance)
+                if distance < 0:
+                    continue #here we want to continue in the same direction
+                distances[cell] = distance
+        try:
+            nearest_cell = get_key(min(distances.values()), distances)
+        except ValueError():
+            raise ValueError("No solution teta loop arrived to a cell that have no near cells in row and and when tried to find in column, didn't find an more advanced cell!")
+        print("Nearest cell in row:", nearest_cell)
+        #print("Nearest cell value: ", self.basic_cells[nearest_cell])
+        return nearest_cell
+
+    def nearest_in_col(self, working_with_cell):
+        distances = {}
+        for cell in self.basic_cells:
+            if cell != working_with_cell and cell[2] == working_with_cell[2]: 
+                distance = int(working_with_cell[1]) - int(cell[1]) #distance is how many dest cell is away from entering cell
+                print("Distance: ", distance)
+                if distance < 0:
+                    distance = -distance #we need absolute value of distance
+                distances[cell] = distance
+        if not distances:
+            nearest_cell_in_row = self.nearest_in_row_if_no_nearest_in_col(working_with_cell) #bas ra7 tkoun hiyye hiyye l entering var aw bl a7ra lli kmonna 3anda lamma sh8alna nearest in row awwal marra
+            self.teta_loop_basic_cells[nearest_cell_in_row] = self.basic_cells[nearest_cell_in_row]
+            return self.nearest_in_col(nearest_cell_in_row)
+        else:
+            nearest_cell = get_key(min(distances.values()), distances)
+            print("Nearest cell returned by nearest_in_col: ", nearest_cell)
+            return nearest_cell
+
+    def nearest_in_row_if_no_nearest_in_col(self, working_with_cell):
+        #first la ndman 2eenno l nearest cell in row ma tkoun l cell lli jina menna bnemna3 l distance tkoun negative
+        #teta----cell_but_no_nearest_in_col----cell_but_no_nearest_in_col----nothing----nothing----cell_with_nearest_in_col
+        distances = {}
+        for cell in self.basic_cells:
+            if cell != working_with_cell and cell[1] == working_with_cell[1]: 
+                distance = int(working_with_cell[2]) - int(cell[2]) #distance is how many dest cell is away from entering cell
+                print("Distance: ", distance)
+                if distance < 0:
+                    continue #here we want to continue in the same direction
                 distances[cell] = distance
         nearest_cell = get_key(min(distances.values()), distances)
         print("Nearest cell in row:", nearest_cell)
-        print("Nearest cell value: ", self.basic_cells[nearest_cell])
-        return nearest_cell
-
-    def nearest_in_col(self, cell_to_workwith):
-        distances = {}
-        for cell in self.basic_cells:
-            if cell != cell_to_workwith and cell[2] == cell_to_workwith[2]: 
-                distance = int(cell[1]) - int(cell_to_workwith[1]) #distance is how many dest cell is away from entering cell
-                print("Distance: ", distance)
-                if distance < 0:
-                    distance = -distance #we need absolute value of distance
-                distances[cell] = distance
-        nearest_cell = get_key(min(distances.values()), distances)
-        print("Nearest cell in col:", nearest_cell)
-        print("Nearest cell value: ", self.basic_cells[nearest_cell])
-        return nearest_cell
+        return str(nearest_cell)
 
     def constructing_teta_loop(self, working_with_cell, entering_var):
+        #self.teta_loop_basic_cells = {} #if we are contructing teta loop for the second, third.. time we should restart from {}, but since contructing_teta_loop recrusion itself we should clearing it in another place
+        #if there is a status where I should start with nearest col
+        #I will test it see the resulting error, then using try nearest_in row w kol code, except l error try nearest_in_col w be2e l code
         nearest_cell = self.nearest_in_row(working_with_cell)
         if nearest_cell != entering_var:
             self.teta_loop_basic_cells[nearest_cell] = self.basic_cells[nearest_cell]
-            nearest_cell = self.nearest_in_col(nearest_cell)
+            nearest_cell = self.nearest_in_col(nearest_cell)#[1] + self.nearest_in_col(nearest_cell)[2]
+            print(f"Nearest cell provided by nearest_in_col: {nearest_cell}")
             if nearest_cell != entering_var:
                 self.teta_loop_basic_cells[nearest_cell] = self.basic_cells[nearest_cell]
                 self.constructing_teta_loop(nearest_cell, entering_var)
             return self.teta_loop_basic_cells
         return "Should always start with entering var as working with cell"
+
+    def find_teta_leaving_var(self):
+        #In this func the cells where we will substract teta will be filtered
+        #teta_value is where the min value
+        #leaving var is where teta_value
+        cells_where_minus_teta = {}
+        c = 0
+        for cell in self.teta_loop_basic_cells:
+            c += 1 
+            if c % 2 == 0:
+                cells_where_minus_teta[cell] = self.teta_loop_basic_cells[cell]
+        print(f"Cells where minus teta {cells_where_minus_teta}")
+        teta_value = min(cells_where_minus_teta.values())
+        print("Teta value; ", teta_value)
+        leaving_var = get_key(teta_value, self.teta_loop_basic_cells)
+        return teta_value, leaving_var
 
     def enter_leave_vars(self):
         entering_var = get_key(max(self.reduced_costs.values()), self.reduced_costs)
@@ -189,37 +255,59 @@ class TransportationModel():
         c = 0
         #we will add entering var to basic cells first, why first because we need from the enetring var to be the nearest cell in a certain stage of teta loop
         #So if its not a basic cell how can it be the nearest cell, so the loop stop
+        self.teta_loop_basic_cells = {} #should do this, because teta_loop_basic_cells can be full of data from a previous self.constructing_teta_loop()
         self.teta_loop_basic_cells[entering_var] = 0
         self.basic_cells[entering_var] = 0
+        print(f"teta_loop_basic_cells when adding entering var {self.teta_loop_basic_cells}")
         #First looping over vbasic cells and filtering cells in the same row of entering_var to find the nearest one
         teta_loop_basic_cells = self.constructing_teta_loop(entering_var, entering_var)
         print(f"Teta loop basic cells: {teta_loop_basic_cells}")
-        teta_value = min(teta_loop_basic_cells.values())
+        teta_value = self.find_teta_leaving_var()[0]
         c = 0
         for cell in teta_loop_basic_cells:
-            print(cell)
+            #print(cell)
             c += 1
             if c % 2 != 0:
                 self.basic_cells[cell] += teta_value
             else:
                 self.basic_cells[cell] -= teta_value
-        leaving_var = get_key(teta_value, teta_loop_basic_cells)
+        leaving_var = get_key(teta_value, self.teta_loop_basic_cells)
+        print(f"Leaving var: {leaving_var}")
         self.basic_cells.pop(leaving_var) #removing leaving variable
+        self.reduced_costs.pop(entering_var) #if not removing entering var from reduced costs the max reduced cost what telled us about optima;ity will still the same and we will never acheive optimality
         print("New basic cells after add/sub teta:  ", self.basic_cells)
         return self.basic_cells        
-   
-    def do_all(self, sources_nb, dests_nb):
-        self.fill_and_balance(sources_nb, dests_nb)
-        self.fill_cells()
-        self.fill_costs()
+
+    def redo(self):
+        self.enter_leave_vars()
         self.fill_Us_Vs()
         self.calc_reduced_costs()
         check = self.check_optimality()
+        print("Check in redo ", check)
+        if check:
+            return "Optimal", self.basic_cells
+        self.redo()
+        
+
+    def do_all(self, sources_nb, dests_nb):
+        self.fill_and_balance(sources_nb, dests_nb)
+        self.fill_cells()
+        
+        cost_type = int(input("Choose the cost type:\n { To choose Cost/unit Enter '0', To choose enter Cost/unit/unit '1' }\n"))
+        try:
+            cost_type = bool(cost_type)
+        except:
+            raise ValueError("Should enter 0 or 1")
+        self.fill_costs(cost_type)
+        self.fill_Us_Vs()
+        self.calc_reduced_costs()
+        check = self.check_optimality()
+        print("Check in do_all: ", check)
         if check: #if True
             return "Optimal"
-        self.entering_leaving_vars()
+        self.redo()
 
-TransportationModel().enter_leave_vars()
-
+#TransportationModel().enter_leave_vars()
+TransportationModel().do_all(3, 4)
 #returned infinite recrusion
 #max reduced cost is always the same
